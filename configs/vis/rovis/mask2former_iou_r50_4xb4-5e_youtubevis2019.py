5cc3aaa3b512f75e990509e1c0bee742af615adf
Zitong Zhan <zitongz3@illinois.edu>
2023-02-15 21:15:23 -0600

    add config files

---
+++ configs/vis/rovis/mask2former_iou_r50_4xb4-5e_youtubevis2019.py
@@ -0,0 +1,254 @@
+_base_ = [
+    '../../_base_/datasets/youtube_vis.py',
+    '../../_base_/default_runtime.py',
+]
+
+# use the same data preprocessor as Mask2Former and MaskTrackRCNN
+data_preprocessor = dict(
+    type='TrackDataPreprocessor',
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    bgr_to_rgb=True,
+    pad_mask=True,
+    pad_size_divisor=32)
+
+num_things_classes = 40
+num_stuff_classes = 0
+num_classes = num_things_classes + num_stuff_classes
+
+model = dict(
+    type='SOLOTrack',
+    data_preprocessor=data_preprocessor,
+    detector=dict(
+        _scope_='mmdet',
+        type='Mask2Former',
+        backbone=dict(
+            type='ResNet',
+            depth=50,
+            num_stages=4,
+            out_indices=(0, 1, 2, 3),
+            frozen_stages=-1,
+            norm_cfg=dict(type='BN', requires_grad=False),
+            norm_eval=True,
+            style='pytorch',
+            init_cfg=dict(
+                type='Pretrained', checkpoint='torchvision://resnet50')),
+        panoptic_head=dict(
+            type='Mask2FormerHead',
+            in_channels=[256, 512, 1024, 2048],  # pass to pixel_decoder inside
+            strides=[4, 8, 16, 32],
+            feat_channels=256,
+            out_channels=256,
+            num_things_classes=num_things_classes,
+            num_stuff_classes=num_stuff_classes,
+            num_queries=100,
+            num_transformer_feat_level=3,
+            pixel_decoder=dict(
+                type='MSDeformAttnPixelDecoder',
+                num_outs=3,
+                norm_cfg=dict(type='GN', num_groups=32),
+                act_cfg=dict(type='ReLU'),
+                encoder=dict(
+                    type='DetrTransformerEncoder',
+                    num_layers=6,
+                    transformerlayers=dict(
+                        type='BaseTransformerLayer',
+                        attn_cfgs=dict(
+                            type='MultiScaleDeformableAttention',
+                            embed_dims=256,
+                            num_heads=8,
+                            num_levels=3,
+                            num_points=4,
+                            im2col_step=64,
+                            dropout=0.0,
+                            batch_first=False,
+                            norm_cfg=None,
+                            init_cfg=None),
+                        ffn_cfgs=dict(
+                            type='FFN',
+                            embed_dims=256,
+                            feedforward_channels=1024,
+                            num_fcs=2,
+                            ffn_drop=0.0,
+                            act_cfg=dict(type='ReLU', inplace=True)),
+                        operation_order=('self_attn', 'norm', 'ffn', 'norm')),
+                    init_cfg=None),
+                positional_encoding=dict(
+                    type='SinePositionalEncoding',
+                    num_feats=128,
+                    normalize=True),
+                init_cfg=None),
+            enforce_decoder_input_project=False,
+            positional_encoding=dict(
+                type='SinePositionalEncoding', num_feats=128, normalize=True),
+            transformer_decoder=dict(
+                type='DetrTransformerDecoder',
+                return_intermediate=True,
+                num_layers=9,
+                transformerlayers=dict(
+                    type='DetrTransformerDecoderLayer',
+                    attn_cfgs=dict(
+                        type='MultiheadAttention',
+                        embed_dims=256,
+                        num_heads=8,
+                        attn_drop=0.0,
+                        proj_drop=0.0,
+                        dropout_layer=None,
+                        batch_first=False),
+                    ffn_cfgs=dict(
+                        embed_dims=256,
+                        feedforward_channels=2048,
+                        num_fcs=2,
+                        act_cfg=dict(type='ReLU', inplace=True),
+                        ffn_drop=0.0,
+                        dropout_layer=None,
+                        add_identity=True),
+                    feedforward_channels=2048,
+                    operation_order=('cross_attn', 'norm', 'self_attn', 'norm',
+                                     'ffn', 'norm')),
+                init_cfg=None),
+            loss_cls=dict(
+                type='CrossEntropyLoss',
+                use_sigmoid=False,
+                loss_weight=2.0,
+                reduction='mean',
+                class_weight=[1.0] * num_classes + [0.1]),
+            loss_mask=dict(
+                type='CrossEntropyLoss',
+                use_sigmoid=True,
+                reduction='mean',
+                loss_weight=5.0),
+            loss_dice=dict(
+                type='DiceLoss',
+                use_sigmoid=True,
+                activate=True,
+                reduction='mean',
+                naive_dice=True,
+                eps=1.0,
+                loss_weight=5.0)),
+        panoptic_fusion_head=dict(
+            type='mmtrack.QueryAsTrID',
+            num_things_classes=num_things_classes,
+            num_stuff_classes=num_stuff_classes,
+            loss_panoptic=None,
+            init_cfg=None),
+        train_cfg=dict(
+            num_points=12544,
+            oversample_ratio=3.0,
+            importance_sample_ratio=0.75,
+            assigner=dict(
+                type='HungarianAssigner',
+                match_costs=[
+                    dict(type='ClassificationCost', weight=2.0),
+                    dict(
+                        type='CrossEntropyLossCost',
+                        weight=5.0,
+                        use_sigmoid=True),
+                    dict(type='DiceCost', weight=5.0, pred_act=True, eps=1.0)
+                ]),
+            sampler=dict(type='MaskPseudoSampler')),
+        test_cfg=dict(
+            panoptic_on=True,
+            # For now, the dataset does not support
+            # evaluating semantic segmentation metric.
+            semantic_on=False,
+            instance_on=True,
+            # max_per_image is for instance segmentation.
+            max_per_image=100,
+            iou_thr=0.8,
+            # In Mask2Former's panoptic postprocessing,
+            # it will filter mask area where score is less than 0.5 .
+            filter_low_score=True),
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint='https://download.openmmlab.com/mmdetection/v2.0/'
+            'mask2former/mask2former_r50_lsj_8x2_50e_coco/'
+            'mask2former_r50_lsj_8x2_50e_coco_20220506_191028-8e96e88b.pth')),
+    track_head=None,
+    tracker=None,
+    img_emb_head=None,
+)
+
+# optimizer mmdet3.x
+embed_multi = dict(lr_mult=1.0, decay_mult=0.0)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW',
+        lr=0.0001 / 4,
+        weight_decay=0.05,
+        eps=1e-8,
+        betas=(0.9, 0.999)),
+    paramwise_cfg=dict(
+        custom_keys={
+            'backbone': dict(lr_mult=0.1, decay_mult=1.0),
+            'query_embed': embed_multi,
+            'query_feat': embed_multi,
+            'level_embed': embed_multi,
+        },
+        norm_decay_mult=0.0),
+    clip_grad=dict(max_norm=0.01, norm_type=2))
+
+# learning policy
+total_epochs = 5
+param_scheduler = dict(
+    type='mmdet.MultiStepLR',
+    begin=0,
+    end=total_epochs,
+    by_epoch=True,
+    milestones=[2, 3],
+    gamma=0.1)
+
+# runtime settings
+train_cfg = dict(
+    type='EpochBasedTrainLoop', max_epochs=total_epochs, val_begin=13)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+# evaluator
+val_evaluator = dict(
+    type='YouTubeVISMetric',
+    metric='youtube_vis_ap',
+    outfile_prefix='./youtube_vis_results',
+    format_only=True)
+test_evaluator = val_evaluator
+
+# overwrite the dataset settings
+img_size = (960, 480)
+train_pipeline = [
+    dict(
+        type='TransformBroadcaster',
+        share_random_params=True,
+        transforms=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='LoadTrackAnnotations',
+                with_instance_id=True,
+                with_mask=True,
+                with_bbox=True),
+            dict(type='mmdet.Resize', scale=img_size, keep_ratio=True),
+            dict(type='mmdet.RandomFlip', prob=0.5),
+        ]),
+    dict(type='PackTrackInputs', ref_prefix='ref', num_key_frames=1)
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='LoadTrackAnnotations',
+        with_instance_id=True,
+        with_mask=True,
+        with_bbox=True),
+    dict(type='mmdet.Resize', scale=img_size, keep_ratio=True),
+    dict(type='PackTrackInputs', pack_single_img=True)
+]
+
+# specify batch size in dataloader
+train_dataloader = dict(
+    batch_size=4,
+    dataset=dict(
+        ref_img_sampler=dict(frame_range=5, ),
+        pipeline=train_pipeline,
+    ))
+val_dataloader = dict(batch_size=1, dataset=dict(pipeline=test_pipeline, ))
+test_dataloader = val_dataloader
